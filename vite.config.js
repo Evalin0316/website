9aed961078969c04346b5a645217cb6c872f070d
Evalin0316 <59949752+Evalin0316@users.noreply.github.com>
2023-10-04 14:22:28 +0800

    feat:adjust vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base:process.env.NODE_ENV === 'production' ? '/website/' : '/',
+  envDir: path.resolve(__dirname, "./env")
 })
